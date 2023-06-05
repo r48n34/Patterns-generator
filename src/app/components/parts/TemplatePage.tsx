9837ad3a8a4b1692b265940619e68f22827aeb25
reemo <r48n34@gmail.com>
2023-06-05 15:58:53 +0800

    Add template search functions

--- src/app/components/parts/TemplatePage.tsx
+++ src/app/components/parts/TemplatePage.tsx
@@ -1,16 +1,40 @@
-import React from 'react';
-import { ScrollArea, Container, Grid } from '@mantine/core';
+import React, { useEffect, useState } from 'react';
+import { ScrollArea, Container, Grid, TextInput } from '@mantine/core';
 import { templateList } from '../../data/templateConfig';
 import TemplateCard from '../template/TemplateCard';
+import { PattenConfig } from '../../interface/shapesConfig';
+import { IconSearch } from '@tabler/icons-react';
 
 function TemplatePage(){
 
+    const [ searchStr, setSearchStr ] = useState<string>("");
+    const [ displayList, setDisplayList ] = useState<PattenConfig[]>(templateList);
+
+    useEffect( () => {
+        const newList = templateList.filter( v => v.title.toLocaleLowerCase().includes( searchStr.toLocaleLowerCase() ));
+        setDisplayList(newList)
+    },[searchStr])
+
     return (
         <>
+            <Container>
+
+            <TextInput
+                // label="Search" 
+                placeholder="Search text"
+                value={searchStr}
+                icon={<IconSearch size="0.8rem" />}
+                onChange={(event) => setSearchStr(event.currentTarget.value)}
+                mt={8}
+                mb={16}
+            />
+
+            </Container>
+            
             <ScrollArea h={"90vh"}>
             <Container>
             <Grid>
-                {templateList.map( v =>
+                {displayList.map( v =>
                     <Grid.Col span={12} key={v.title}>
                         <TemplateCard data={v} showsDelete={false} showsEdit={false}/>
                     </Grid.Col>
